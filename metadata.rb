870e78ae1134944a03a86e4164673adca28b6400
Christian Fischer <sw-dev@computerlyrik.de>
2013-10-21 18:14:59 +0200

    version up!

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef-cookbooks@computerlyrik.de'
 license          'Apache 2.0'
 description      'Installs/Configures phpldapadmin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'apache2'
 depends 'openldap'
